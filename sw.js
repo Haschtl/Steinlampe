aee85be38a2caf7662f2c0a56a150030442ceeda
haschtl <haschtl@pm.me>
2025-12-06 20:44:12 +0100

    PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'quarzlampe-v1';
+const ASSETS = [
+  './',
+  './webble.html',
+  './manifest.json',
+  './icon-lamp.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+});
